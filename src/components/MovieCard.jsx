99d9979129b986461d5d200657b3e83fbde07ca5
Sanja Daic <sanja.daic86@gmail.com>
2025-11-14 14:57:47 +0100

    dynamically adding rating color

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,16 @@ const MovieCard = ({ movie }) => {
     event.target.src = "images/default.jpg";
   };
 
+  const getRatingClass = (rating) => {
+    if (rating >= 8) {
+      return "movie__card-rating--good";
+    } else if (rating >= 5 && rating < 8) {
+      return "movie__card-rating--ok";
+    } else if (rating < 4) {
+      return "movie__card-rating--bad";
+    }
+  };
+
   return (
     <div className="movie__card">
       <img
@@ -14,7 +24,9 @@ const MovieCard = ({ movie }) => {
       <div className="movie__card-info">
         <h3 className="movie__card-title">{movie.title}</h3>
         <p className="movie__card-genre">{movie.genre}</p>
-        <p className="movie__card-rating">{movie.rating}</p>
+        <p className={`movie__card-rating ${getRatingClass(movie.rating)}`}>
+          {movie.rating}
+        </p>
       </div>
     </div>
   );
