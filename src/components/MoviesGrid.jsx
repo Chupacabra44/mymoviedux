ba90b8005fd9c13ca2414e3218c049d10b6eec32
Sanja Daic <sanja.daic86@gmail.com>
2025-11-19 11:38:33 +0100

    Search state and Search bar logic

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -3,6 +3,7 @@ import MovieCard from "./MovieCard";
 
 const MoviesGrid = () => {
   const [movies, setMovies] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -17,10 +18,27 @@ const MoviesGrid = () => {
     fetchMovies();
   }, []);
 
+  const handleSearch = (event) => {
+    setSearchMovie(event.target.value);
+  };
+
+  const filteredMovie = movies.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(searchMovie) ||
+      movie.genre.toLowerCase().includes(searchMovie)
+  );
+
   return (
     <div className="container">
+      <input
+        onChange={handleSearch}
+        className="input"
+        type="text"
+        placeholder="Search movies ..."
+        value={searchMovie}
+      />
       <div className="movie__container">
-        {movies.map((movie) => (
+        {filteredMovie.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
