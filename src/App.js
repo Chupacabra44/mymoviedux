e89973aab09083fbe5abd953c8f4de5b8fb47d73
Sanja Daic <sanja.daic86@gmail.com>
2025-11-20 09:02:57 +0100

    Configuring the Router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import "./styles.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <Header />
-        <MoviesGrid />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<WatchList />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
